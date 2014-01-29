dbea9cabe6dba19c8e9f90b680a573c328932a9b
Lance Speelmon <lance@speelmon.com>
2014-01-29 14:54:05 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EasyProposal::Application.config.secret_key_base = '61f34c721ba4ec74e176d5c6b74aa973ad0c7cd3408590b05db719baeaa7176d8df7c8ae9ef825eb6be49f7791118c8e426ba1ec9a438c0e4524ed76bc8c36d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EasyProposal::Application.config.session_store :cookie_store, key: '_easy_proposal_session'
