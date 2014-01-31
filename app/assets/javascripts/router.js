83aa5641f24f61e9d35c1881b5aec598d9b9efb3
Lance Speelmon <lance@speelmon.com>
2014-01-31 10:54:44 -0700

    redrect index route to proposals

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,7 +1,14 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 EasyProposal.Router.map(function() {
-  this.resource("proposals", { path: '/' }, function() {
+  this.resource("proposals", function() {
     this.resource("proposal", { path: ":docId" });
   });
+});
+
+// redrect index route to proposals
+EasyProposal.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('proposals');
+  }
 });
